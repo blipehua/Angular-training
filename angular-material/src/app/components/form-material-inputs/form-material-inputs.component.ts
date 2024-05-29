47811420348656d76a25a788e9c308f1988dac29
Brayan Lipe Huamaní <blipehua@emeal.nttdata.com>
2024-05-28 23:15:37 -0500

    Angular training: Styling, form control y validators.

--- angular-material/src/app/components/form-material-inputs/form-material-inputs.component.ts
+++ angular-material/src/app/components/form-material-inputs/form-material-inputs.component.ts
@@ -2,29 +2,28 @@ import { Component } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, Validators } from '@angular/forms';
 import { merge } from 'rxjs';
-
 @Component({
   selector: 'app-form-material-inputs',
   templateUrl: './form-material-inputs.component.html',
   styleUrl: './form-material-inputs.component.css'
 })
 export class FormMaterialInputsComponent {
-  // textoIzquierdo = new FormControl('', [Validators.required, Validators.email]);
-  // errorMessage = '';
+  texto = new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')]);
+  errorMessage = '';
 
-  // constructor() {
-  //   merge(this.textoIzquierdo.statusChanges, this.textoIzquierdo.valueChanges)
-  //     .pipe(takeUntilDestroyed())
-  //     .subscribe(() => this.updateErrorMessage());
-  // }
+  constructor() {
+    merge(this.texto.statusChanges, this.texto.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
 
-  // updateErrorMessage() {
-  //   if (this.textoIzquierdo.hasError('required')) {
-  //     this.errorMessage = 'Debes de ingresar un valor';
-  //   } else if (this.textoIzquierdo.hasError('textoIzquierdo')) {
-  //     this.errorMessage = 'Texto de error';
-  //   } else {
-  //     this.errorMessage = '';
-  //   }
-  // }
+  updateErrorMessage() {
+    if (this.texto.hasError('required')) {
+      this.errorMessage = 'Debes ingresar un texto';
+    } else if (this.texto.invalid) {
+      this.errorMessage = 'Texto inválido, debes ingresar letras';
+    } else {
+      this.errorMessage = '';
+    }
+  }
 }
